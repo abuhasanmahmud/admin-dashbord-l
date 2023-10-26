2c03734696de20d6d3390a0f3c51f25808f40202
abuhasanmahmud <mahmud.me420@gmail.com>
2023-10-26 10:07:47 +0600

    Feat(v-1) product searching in product page

--- src/app/components/products/ProductTable.tsx
+++ src/app/components/products/ProductTable.tsx
@@ -1,189 +1,156 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-const ProductTable = ({
-  products,
-  handelProductUpdata,
-  setIsDeleteModal,
-  setProductId,
-  setProductDrawer,
-  setProductDetails,
-}: any) => {
+const ProductTable = ({ products, handelProductUpdata, setIsDeleteModal, setProductId }: any) => {
   const path = usePathname();
   return (
     <>
-      <section className="mx-auto w-full max-w-7xl px-4 py-4">
-        <div className="flex flex-col space-y-4  md:flex-row md:items-center md:justify-between md:space-y-0">
-          <div>
-            <h2 className="text-lg font-semibold">Products</h2>
-            <p className="mt-1 text-sm text-gray-700">
-              This is a list of all Products. You can add new products, edit or delete existing ones.
-            </p>
-          </div>
+      <div className="mt-6 flex flex-col">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className=" py-3.5 mx-2 text-sm font-normal text-gray-700">
+                      <span>Image</span>
+                    </th>
+                    <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
+                      <span>Name</span>
+                    </th>
+                    <th scope="col" className=" py-3.5 text-left text-sm font-normal text-gray-700">
+                      Category
+                    </th>
+                    <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
+                      Price
+                    </th>
 
-          {path === "/product" && (
-            <>
-              <div>
-                <button
-                  onClick={(e) => {
-                    setProductDrawer(true), setProductDetails({});
-                  }}
-                  type="button"
-                  className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                >
-                  Add new product
-                </button>
-              </div>
-            </>
-          )}
-        </div>
-        <div className="mt-6 flex flex-col">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th scope="col" className=" py-3.5 mx-2 text-sm font-normal text-gray-700">
-                        <span>Image</span>
-                      </th>
-                      <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
-                        <span>Name</span>
-                      </th>
-                      <th scope="col" className=" py-3.5 text-left text-sm font-normal text-gray-700">
-                        Category
-                      </th>
-                      <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
-                        Price
-                      </th>
+                    <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
+                      Status
+                    </th>
 
-                      <th scope="col" className=" py-3.5 text-center text-sm font-normal text-gray-700">
-                        Status
-                      </th>
+                    {path === "/products" ? (
+                      <>
+                        {" "}
+                        <th
+                          scope="col"
+                          className=" py-3.5 text-center text-sm font-normal text-gray-700"
+                        >
+                          Action
+                        </th>{" "}
+                      </>
+                    ) : (
+                      <>
+                        {" "}
+                        <th
+                          scope="col"
+                          className=" py-3.5 text-center text-sm font-normal text-gray-700"
+                        >
+                          Quantity
+                        </th>
+                      </>
+                    )}
+                    <th scope="col" className="relative px-4 py-3.5">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {products?.map((item) => (
+                    <tr key={item.name}>
+                      <td className="whitespace-nowrap text-center mx-2  py-4">
+                        <div className="text-sm text-gray-900 flex justify-center">
+                          <img
+                            className="h-10 w-10 rounded-full object-cover"
+                            src="https://i.ibb.co/g9sxg8L/lorem-ipsum-dolor-sit-amet-1.png"
+                            alt=""
+                          />
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap text-center py-4">
+                        <div className="text-sm text-center text-gray-900 ">{item?.name}</div>
+                      </td>
+                      <td className="whitespace-nowrap  py-4">
+                        <div className="text-sm text-gray-900 ">{item?.category}</div>
+                      </td>
+                      <td className="whitespace-nowrap  py-4">
+                        <div className="text-sm text-gray-900 text-center ">{item?.price}</div>
+                      </td>
+                      <td className="whitespace-nowrap text-center py-4">
+                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                          {item?.status}
+                        </span>
+                      </td>
 
-                      {path === "/products" ? (
+                      {path === "/product" ? (
                         <>
-                          {" "}
-                          <th
-                            scope="col"
-                            className=" py-3.5 text-center text-sm font-normal text-gray-700"
-                          >
-                            Action
-                          </th>{" "}
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right flex justify-center  items-center gap-2 text-sm font-medium sm:pr-0 mt-2">
+                            <a
+                              onClick={() => handelProductUpdata(item)}
+                              className="text-indigo-600 hover:text-indigo-900 cursor-pointer "
+                            >
+                              Edit<span className="sr-only"> {item.name}</span>
+                            </a>
+
+                            <a
+                              onClick={() => {
+                                setIsDeleteModal(true), setProductId(item._id.toString());
+                              }}
+                              className="text-indigo-600 hover:text-indigo-900"
+                            >
+                              <RiDeleteBin5Fill className=" cursor-pointer " />
+                            </a>
+                          </td>
                         </>
                       ) : (
                         <>
-                          {" "}
-                          <th
-                            scope="col"
-                            className=" py-3.5 text-center text-sm font-normal text-gray-700"
-                          >
-                            Quantity
-                          </th>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center ">
+                            {item.quantity} 00
+                          </td>
                         </>
                       )}
-                      <th scope="col" className="relative px-4 py-3.5">
-                        <span className="sr-only">Edit</span>
-                      </th>
                     </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {products?.map((item) => (
-                      <tr key={item.name}>
-                        <td className="whitespace-nowrap text-center mx-2  py-4">
-                          <div className="text-sm text-gray-900 flex justify-center">
-                            <img
-                              className="h-10 w-10 rounded-full object-cover"
-                              src="https://i.ibb.co/g9sxg8L/lorem-ipsum-dolor-sit-amet-1.png"
-                              alt=""
-                            />
-                          </div>
-                        </td>
-                        <td className="whitespace-nowrap text-center py-4">
-                          <div className="text-sm text-center text-gray-900 ">{item?.name}</div>
-                        </td>
-                        <td className="whitespace-nowrap  py-4">
-                          <div className="text-sm text-gray-900 ">{item?.category}</div>
-                        </td>
-                        <td className="whitespace-nowrap  py-4">
-                          <div className="text-sm text-gray-900 text-center ">{item?.price}</div>
-                        </td>
-                        <td className="whitespace-nowrap text-center py-4">
-                          <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                            {item?.status}
-                          </span>
-                        </td>
-
-                        {path === "/product" ? (
-                          <>
-                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right flex justify-center  items-center gap-2 text-sm font-medium sm:pr-0 mt-2">
-                              <a
-                                onClick={() => handelProductUpdata(item)}
-                                className="text-indigo-600 hover:text-indigo-900 cursor-pointer "
-                              >
-                                Edit<span className="sr-only"> {item.name}</span>
-                              </a>
-
-                              <a
-                                onClick={() => {
-                                  setIsDeleteModal(true), setProductId(item._id.toString());
-                                }}
-                                className="text-indigo-600 hover:text-indigo-900"
-                              >
-                                <RiDeleteBin5Fill className=" cursor-pointer " />
-                              </a>
-                            </td>
-                          </>
-                        ) : (
-                          <>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center ">
-                              {item.quantity} 00
-                            </td>
-                          </>
-                        )}
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center pt-6">
-          <a href="#" className="mx-1 cursor-not-allowed text-sm font-semibold text-gray-900">
-            <span className="hidden lg:block">&larr; Previous</span>
-            <span className="block lg:hidden">&larr;</span>
-          </a>
-          <a
-            href="#"
-            className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
-          >
-            1
-          </a>
-          <a
-            href="#"
-            className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
-          >
-            2
-          </a>
-          <a
-            href="#"
-            className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
-          >
-            3
-          </a>
-          <a
-            href="#"
-            className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
-          >
-            4
-          </a>
-          <a href="#" className="mx-2 text-sm font-semibold text-gray-900">
-            <span className="hidden lg:block">Next &rarr;</span>
-            <span className="block lg:hidden">&rarr;</span>
-          </a>
-        </div>
-      </section>
+      </div>
+      <div className="flex items-center justify-center pt-6">
+        <a href="#" className="mx-1 cursor-not-allowed text-sm font-semibold text-gray-900">
+          <span className="hidden lg:block">&larr; Previous</span>
+          <span className="block lg:hidden">&larr;</span>
+        </a>
+        <a
+          href="#"
+          className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
+        >
+          1
+        </a>
+        <a
+          href="#"
+          className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
+        >
+          2
+        </a>
+        <a
+          href="#"
+          className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
+        >
+          3
+        </a>
+        <a
+          href="#"
+          className="mx-1 flex items-center rounded-md border border-gray-400 px-3 py-1 text-gray-900 hover:scale-105"
+        >
+          4
+        </a>
+        <a href="#" className="mx-2 text-sm font-semibold text-gray-900">
+          <span className="hidden lg:block">Next &rarr;</span>
+          <span className="block lg:hidden">&rarr;</span>
+        </a>
+      </div>
     </>
   );
 };
--- src/app/components/products/Products.tsx
+++ src/app/components/products/Products.tsx
@@ -1,26 +1,71 @@
 "use client";
 
 import ProductTable from "./ProductTable";
+import { useState, useEffect } from "react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { FiSearch } from "react-icons/fi";
 
-import { useState } from "react";
+import { Combobox } from "@headlessui/react";
 import ProductDrawer from "../drawer/productDrawer";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useMyContext } from "../context/myContext";
 import DeleteModal2 from "../modal/DeleteModal2";
-
+const people = [
+  { id: 1, name: "Leslie Alexander" },
+  { id: 2, name: " Alexander " },
+  { id: 3, name: "p Alexander" },
+  { id: 4, name: "zz Alexander" },
+];
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 const Products = ({ allProducts }: any) => {
+  const router = useRouter();
   const path = usePathname();
-  console.log("products", allProducts);
+  // console.log("products", allProducts);
   const [productDrawer, setProductDrawer] = useState(false);
   const [productDetails, setProductDetails] = useState({});
   const [productId, setProductId] = useState("");
-
   const { setIsDeleteModal } = useMyContext();
 
+  //handel product update
   const handelProductUpdata = (item: any) => {
     setProductDetails(item);
     setProductDrawer(true);
   };
+
+  //handle filter by category
+  const [query, setQuery] = useState("");
+  const [selectedPerson, setSelectedPerson] = useState(null);
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+  //Handel product searching
+  const [searchText, setSearchText] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(allProducts);
+
+  const handleSearch = (event) => {
+    if (event.key === "Enter") {
+      const newFilteredProducts = allProducts.filter((product) =>
+        product.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilteredProducts(newFilteredProducts);
+    }
+  };
+
+  useEffect(() => {
+    if (searchText.length === 0) {
+      console.log("search text empty");
+      setFilteredProducts(allProducts);
+    }
+  }, [searchText, allProducts]);
+
+  // console.log("allProducts", allProducts, "filter product=", filteredProducts);
+
   return (
     <>
       <ProductDrawer
@@ -29,14 +74,160 @@ const Products = ({ allProducts }: any) => {
         productDetails={productDetails}
       />
       <DeleteModal2 productId={productId} />
-      <ProductTable
-        products={allProducts}
-        handelProductUpdata={handelProductUpdata}
-        setIsDeleteModal={setIsDeleteModal}
-        setProductId={setProductId}
-        setProductDetails={setProductDetails}
-        setProductDrawer={setProductDrawer}
-      />
+
+      <section className="mx-auto w-full max-w-7xl px-4 py-4">
+        <div className="flex flex-col space-y-4  md:flex-row md:items-center md:justify-between md:space-y-0">
+          <div className="basis-1/2">
+            <h2 className="text-lg font-semibold">Products</h2>
+            <div>
+              <div className="mt-2 relative ">
+                <input
+                  type="text"
+                  placeholder="Search products"
+                  value={searchText}
+                  onKeyPress={handleSearch}
+                  onChange={(e) => setSearchText(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-7"
+                />
+                <p className="absolute top-2 left-1">
+                  <FiSearch size={20} />
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {path === "/product" && (
+            <>
+              <div>
+                <button
+                  onClick={(e) => {
+                    setProductDrawer(true), setProductDetails({});
+                  }}
+                  type="button"
+                  className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                >
+                  Add new product
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="flex gap-4 py-4 mt-2 flex-col sm:flex-row">
+          <div className="basis-1/2">
+            <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
+              <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                Filter By Category
+              </Combobox.Label>
+              <div className="relative mt-2">
+                <Combobox.Input
+                  className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(event) => setQuery(event.target.value)}
+                  displayValue={(person) => person?.name}
+                />
+                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                  <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </Combobox.Button>
+
+                {filteredPeople.length > 0 && (
+                  <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {filteredPeople.map((person) => (
+                      <Combobox.Option
+                        key={person.id}
+                        value={person}
+                        className={({ active }) =>
+                          classNames(
+                            "relative cursor-default select-none py-2 pl-3 pr-9",
+                            active ? "bg-indigo-600 text-white" : "text-gray-900"
+                          )
+                        }
+                      >
+                        {({ active, selected }) => (
+                          <>
+                            <span className={classNames("block truncate", selected && "font-semibold")}>
+                              {person.name}
+                            </span>
+
+                            {selected && (
+                              <span
+                                className={classNames(
+                                  "absolute inset-y-0 right-0 flex items-center pr-4",
+                                  active ? "text-white" : "text-indigo-600"
+                                )}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            )}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    ))}
+                  </Combobox.Options>
+                )}
+              </div>
+            </Combobox>
+          </div>
+
+          <div className="basis-1/2">
+            <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
+              <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                Filter By Prize
+              </Combobox.Label>
+              <div className="relative mt-2">
+                <Combobox.Input
+                  className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(event) => setQuery(event.target.value)}
+                  displayValue={(person) => person?.name}
+                />
+                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                  <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </Combobox.Button>
+
+                {filteredPeople.length > 0 && (
+                  <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {filteredPeople.map((person) => (
+                      <Combobox.Option
+                        key={person.id}
+                        value={person}
+                        className={({ active }) =>
+                          classNames(
+                            "relative cursor-default select-none py-2 pl-3 pr-9",
+                            active ? "bg-indigo-600 text-white" : "text-gray-900"
+                          )
+                        }
+                      >
+                        {({ active, selected }) => (
+                          <>
+                            <span className={classNames("block truncate", selected && "font-semibold")}>
+                              {person.name}
+                            </span>
+
+                            {selected && (
+                              <span
+                                className={classNames(
+                                  "absolute inset-y-0 right-0 flex items-center pr-4",
+                                  active ? "text-white" : "text-indigo-600"
+                                )}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            )}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    ))}
+                  </Combobox.Options>
+                )}
+              </div>
+            </Combobox>
+          </div>
+        </div>
+        <ProductTable
+          products={filteredProducts}
+          handelProductUpdata={handelProductUpdata}
+          setIsDeleteModal={setIsDeleteModal}
+          setProductId={setProductId}
+        />
+      </section>
     </>
   );
 };
